3f7e09fe0ed7d29508722b9282e3ee773ec29784
dMacGit <playerminusone@gmail.com>
2015-02-13 21:51:43 +1300

    Finished the problem
    - Added a few helper toString methods
    - Calculated Hours and Minutes remaining
    - Added alternative display format methods

--- src/core_201_countdown/DayCountdown.java
+++ src/core_201_countdown/DayCountdown.java
@@ -9,50 +9,18 @@ public class DayCountdown
 {
 	//The DayCountdown class
 	
-	private Date CurrentDateInTime;
-	private Date SpecialDateInTime;
-	
+	private Countdown newCountdownDate;
 	
 	public DayCountdown(int day, int month, int year)
 	{
 		/* Takes as default three integer inputs as argument for the
 		 * day in which to calculate the days remaining!
-		 */
+		 */	
 		
-		/*DateFormat specialDateFormatInstance = DateFormat.getDateInstance();
-		DateFormat currentDateformatInstance = DateFormat.getDateTimeInstance();
+		this.newCountdownDate = calculateTimeUntillDate(day, month, year);
 		
-		try 
-		{
-			SpecialDateInTime = specialDateFormatInstance.parse(new String(day+"/"+month+"/"+year));
-			
-			System.out.println("Current date is: "+SpecialDateInTime);
-			Calendar newCalendar = Calendar.getInstance();
-			Calendar specialCalendar = Calendar.getInstance();
-			specialCalendar.set(year, month, day);
-			
-			if(newCalendar.before(specialCalendar))
-			{
-				System.out.println("The current calendar date ["+newCalendar.toString()+" is before the targeted Calendar date: "+specialCalendar.toString());
-			}
-			else System.out.println("The current calendar date ["+new Date(newCalendar.getTimeInMillis())+" was not before the targeted Calendar date: "+new Date(specialCalendar.getTimeInMillis()));
-			
-			
-			
-			
-		}
-		catch (ParseException e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+		System.out.println(this.newCountdownDate.returnCountdown());
 		
-		calculateTimeUntillDate(day, month, year);
-		
-		//Date currentDate = currentDateformatInstance.get
-		//DateFormat.getDateInstance().parse(new String(day+"/"+month+"/"+year));
-		//format.format(new Date(new String(day+"/"+month+"/"+year)));
-		//System.currentTimeMillis();
 	}
 	
 	private Countdown calculateTimeUntillDate(int dayArg, int monthArg, int yearArg)
@@ -62,34 +30,36 @@ public class DayCountdown
 	
 	public class Countdown
 	{
-		private final int theDayOfTheMonth;
-		private final int theMonthOfTheYear;
-		private final int theYear;
+		private static final long MILLISECONDS_PER_SECOND = 1000;
+		private static final long MILLISECONDS_PER_MINUTE = 60 * MILLISECONDS_PER_SECOND;
+		private static final long MILLISECONDS_PER_HOUR = 60 * MILLISECONDS_PER_MINUTE;
+		private static final long MILLISECONDS_PER_DAY = MILLISECONDS_PER_HOUR*24;
 		
 		private Calendar CurrentCalendarTime;
 		private Calendar SpecialCalendarTime;
 		
+		private int CountdownDays = 0;
+		private int RemainderHours = 0;
+		private int RemainderMinutes = 0;
+		
 		public Countdown(int day, int month, int year)
 		{
-			
-			
 			Calendar currentCalendarTime = Calendar.getInstance();	// Grabbing the Calendar Instance for the current Date
 			
 			CurrentCalendarTime = currentCalendarTime;
+			SpecialCalendarTime = dateValidation(day, month, year);
 			
-			SpecialCalendarTime = dateValidation(day, month, year, currentCalendarTime);
-				this.theDayOfTheMonth = day;
-				this.theMonthOfTheYear = month;
-				this.theYear = year;
-				if(CurrentCalendarTime.before(SpecialCalendarTime))
-				{
-					System.out.println("The current calendar date ["+calendarToString_WrittenDateOnly(CurrentCalendarTime)+" is before the targeted Calendar date: "+calendarToString_WrittenDateOnly(SpecialCalendarTime));
-				}
-				else System.out.println("The current calendar date ["+calendarToString_CompactDateOnly(CurrentCalendarTime)+"] was not before the targeted Calendar date: ["+calendarToString_CompactDateOnly(SpecialCalendarTime)+"]");
+			performCountdownInDays();
+			
+			if(CurrentCalendarTime.before(SpecialCalendarTime))
+			{
+				System.out.println("The current calendar date [ "+calendarToString_CompactDateOnly(CurrentCalendarTime)+" ] <====> The targeted countdown date: [ "+calendarToString_CompactDateOnly(SpecialCalendarTime)+" ]");
+			}
+			else System.out.println("The current calendar date ["+calendarToString_CompactDateOnly(CurrentCalendarTime)+"] was not before the targeted Calendar date: ["+calendarToString_CompactDateOnly(SpecialCalendarTime)+"]");
 			
 		}
 		
-		private Calendar dateValidation(int day, int month, int year, Calendar currentCalendar)
+		private Calendar dateValidation(int day, int month, int year)
 		{
 			Calendar specialCalendar = Calendar.getInstance();		// Grabbing the Calendar Instance for the special Date
 			specialCalendar.clear();
@@ -101,8 +71,38 @@ public class DayCountdown
 			
 			return specialCalendar;
 		}
+		
+		/*
+		 * In this method we will be calculating the days until the specified date in time!
+		 */
+		private void performCountdownInDays()
+		{
+			//Calculate the time between calendar dates in milliseconds first
+			final long timeBetween = SpecialCalendarTime.getTimeInMillis() - CurrentCalendarTime.getTimeInMillis();
+			//Next value calculated is just to make the remainder calculations easier to read
+			final long ramainderInDay = timeBetween % MILLISECONDS_PER_DAY;
+			
+			CountdownDays = (int) (timeBetween / MILLISECONDS_PER_DAY);									//<------	The days remaining [The solution]
+			
+			//This code below is just extra calculations that I wanted to find!
+			RemainderHours = (int) ((ramainderInDay)/MILLISECONDS_PER_HOUR);							//<------ 	The Minutes remaining [Not required]
+			RemainderMinutes = (int) ((ramainderInDay%MILLISECONDS_PER_HOUR)/MILLISECONDS_PER_MINUTE);	//<------ 	The Seconds remaining [Again Not required]
+		}
+		
+		/*
+		 * A glorified toString Method
+		 */
+		public String returnCountdown()
+		{
+			return new String("Countdown until the date: "+this.CountdownDays+" Days "+RemainderHours+" Hours & "+this.RemainderMinutes+" Minutes");
+		}
 	}
 	
+	//=========================================== Print Methods ================================================
+	
+	/*
+	 * Below are a few helper methods for printing out the Calendar variables in a format I can read easily.
+	 */
 	private String calendarToString_WrittenDateOnly(Calendar calendarArgument)
 	{
 		//Print out the values for the calendar date without the time portion!
@@ -121,9 +121,11 @@ public class DayCountdown
 		return new String(calendarArgument.getTime().toString());
 	}
 	
+	/*
+	 * The dayFormatter formats the integer argument into the relevant day of the week in name form
+	 */
 	private String dayFormatter(int dayToFormat)
 	{
-		System.out.println("The day to format: "+dayToFormat);
 		switch(dayToFormat)
 		{
 			case 1 : return new String("Sunday");
@@ -145,6 +147,9 @@ public class DayCountdown
 		}
 	}
 	
+	/*
+	 * This monthFormatter method performs the same procedure as the dayFormatter, just on Month values
+	 */
 	private String monthFormatter(int monthToFormat)
 	{
 		switch(monthToFormat)
@@ -178,6 +183,9 @@ public class DayCountdown
 		}
 	}
 	
+	/*
+	 * The Suffixify method returns a suffix to the number of the month inputed!
+	 */
 	private String suffixify(int dayOfTheMonth)
 	{
 		//1st, 21st, 31st
--- src/core_201_countdown/main.java
+++ src/core_201_countdown/main.java
@@ -2,15 +2,36 @@ package core_201_countdown;
 
 public class main
 {
+	
+	/*------------------------------------------------
+	 * 				Day Countdown <EASY>
+	 * -----------------------------------------------
+	 * 
+	 * My solution to the daily programming task
+	 * for [2015-02-9] over at reddit/r/dailyprogramer
+	 * 
+	 * ================================================
+	 * ===   Short link: [http://bit.ly/1D2H88M]
+	 * ================================================
+	 */
+	
 	/*
 	 * This program is designed to take a day of the year as Three integer
-	 * values and give as output the number of days left until that date! 
+	 * values and give as output the number of days left until that date!
+	 * 
+	 * 
 	 */
 	public static void main(String[] args)
 	{
-		
-		
-		
-		DayCountdown counter = new DayCountdown(29,06,1987);
+
+		DayCountdown countDownOne = new DayCountdown(4,7,2015);
+		DayCountdown countDownTwo = new DayCountdown(31,10,2015);
+		DayCountdown countDownThree = new DayCountdown(24,12,2015);
+		DayCountdown countDownFour = new DayCountdown(1,1,2016);
+		DayCountdown countDownFive = new DayCountdown(9,2,2016);
+		DayCountdown countDownSix = new DayCountdown(1,1,2020);
+		DayCountdown countDownSeven = new DayCountdown(9,2,2020);
+		DayCountdown countDownEight = new DayCountdown(1,3,2020);
+		DayCountdown countDownNine = new DayCountdown(9,2,3015);
 	}
 }
